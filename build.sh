84c117f2246797b1c98f0df67e5e645a9394806a
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-11-27 16:31:04 +0100

    Fixed build

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 rm -r ./publish
 set -e
-dotnet restore
-dotnet build
-# dotnet test src/KitchenResponsibleServiceTests/KitchenResponsibleServiceTests.csproj --no-build --no-restore
 dotnet publish Hjerpbakk.PoorMansServiceDiscovery/Hjerpbakk.PoorMansServiceDiscovery.csproj -o ../publish -c Release
 docker build -t service-discovery .
